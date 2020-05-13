c54d946f6c3874fc7edac73d7bd0a4b9716a4214
Olve Drageset <olve.drageset@gmail.com>
2020-05-13 15:20:51 +0200

    started work on PUT and POST methods for subsets

--- src/main/java/no/ssb/subsetsservice/SubsetsController.java
+++ src/main/java/no/ssb/subsetsservice/SubsetsController.java
@@ -1,16 +1,14 @@
 package no.ssb.subsetsservice;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SubsetsController {
 
-    private static final String LDS_PROD = "http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset";
-    private static final String LDS_LOCAL = "http://localhost:9090/ns/ClassificationSubset";
+    static final String LDS_PROD = "http://lds-klass.klass.svc.cluster.local/ns/ClassificationSubset";
+    static final String LDS_LOCAL = "http://localhost:9090/ns/ClassificationSubset";
     private static String LDS_SUBSET_API = "";
 
     private static final String KLASS_CODES_API = "https://data.ssb.no/api/klass/v1/classifications";
@@ -25,33 +23,52 @@ public class SubsetsController {
         }
     }
 
-    @RequestMapping("/v1/subsets")
+    @GetMapping("/v1/subsets")
     public ResponseEntity<String> getSubsets() {
         return getFrom(LDS_SUBSET_API, "");
     }
 
-    @RequestMapping("/v1/subsets/{id}")
+    @PostMapping("/v1/subsets")
+    public ResponseEntity<String> postSubsets(@RequestBody String subsetsJson) {
+        return postTo(LDS_SUBSET_API, "", subsetsJson);
+    }
+
+    @GetMapping("/v1/subsets/{id}")
     public ResponseEntity<String> getSubset(@PathVariable("id") String id) {
         return getFrom(LDS_SUBSET_API, "/"+id);
     }
 
-    @RequestMapping("/v1/subsets?schema")
+    @PutMapping("/v1/subsets/{id}")
+    public ResponseEntity<String> putSubset(@PathVariable("id") String id, @RequestBody String subsetJson) {
+        return putTo(LDS_SUBSET_API, "/"+id, subsetJson);
+    }
+
+    @GetMapping("/v1/subsets?schema")
     public ResponseEntity<String> getSchema(){
         return getFrom(LDS_SUBSET_API,"/?schema");
     }
 
-    @RequestMapping("/v1/codes")
+    @GetMapping("/v1/codes")
     public ResponseEntity<String> getCodes(){
         return getFrom(KLASS_CODES_API, ".json");
     }
 
-    @RequestMapping("/v1/codes/{id}")
+    @GetMapping("/v1/codes/{id}")
     public ResponseEntity<String> getCode(@PathVariable("id") String id){
         return getFrom(KLASS_CODES_API, "/"+id+".json");
     }
 
-    private static ResponseEntity<String> getFrom(String apiBase, String additional)
+    static ResponseEntity<String> getFrom(String apiBase, String additional)
     {
         return new RestTemplate().getForEntity(apiBase + additional, String.class);
+    }
+
+    static ResponseEntity<String> postTo(String apiBase, String additional, String json){
+        //TODO: For each subset in json of subsets, put indnividually to LDS ?
+        return new RestTemplate().postForEntity(apiBase+additional, json, String.class);
+    }
+
+    static ResponseEntity<String> putTo(String apiBase, String additional, String json){
+        return new RestTemplate().postForEntity(apiBase+additional, json, String.class);
     }
 }
