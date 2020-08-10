90864811a45b8d71271d756028fe07f13542d856
Olve Drageset <olve.drageset@gmail.com>
2020-08-10 15:11:39 +0200

    fix issue with sorting of the version list when finding versionValidTo

--- src/main/java/no/ssb/subsetsservice/SubsetsController.java
+++ src/main/java/no/ssb/subsetsservice/SubsetsController.java
@@ -283,8 +283,8 @@ public class SubsetsController {
                     ArrayList<JsonNode> versionList = new ArrayList<>(versionLastUpdatedMap.size());
                     versionLastUpdatedMap.forEach((versionInt, versionJsonNode) -> versionList.add(versionJsonNode));
                     LOG.debug("versionList size: "+versionList.size());
-                    versionList.sort(Comparator.comparing(v -> v.get(Field.VERSION_VALID_FROM).asText())); // TODO: This is sorted wrong way around
-                    String validTo = ""; //TODO: This is a mistake.
+                    versionList.sort((s1, s2) -> s2.get(Field.VERSION_VALID_FROM).asText().compareTo(s1.get(Field.VERSION_VALID_FROM).asText())); // TODO: This is repeated code
+                    String validTo = ""; //TODO: This is potentially a mistake.
                     for (int i = 0; i < versionList.size(); i++) {
                         ObjectNode editableSubset = versionList.get(i).deepCopy();
                         ArrayNode resolvedCodes = resolveURNs(editableSubset, validTo);
