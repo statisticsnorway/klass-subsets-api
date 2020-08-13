5316e3fb5c1de36cec959df608a30840a7d8c516
Olve Drageset <olve.drageset@gmail.com>
2020-08-13 14:14:55 +0200

    clean subset version test. Final test strings

--- src/test/java/no/ssb/subsetsservice/UtilsTest.java
+++ src/test/java/no/ssb/subsetsservice/UtilsTest.java
@@ -1,66 +1,68 @@
 package no.ssb.subsetsservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
 
-    String s0 = "";
-    String s1 = "a";
-    String s2 = "A";
-    String s3 = "!";
-    String s4 = ",1";
-    String s5 = "2-";
-    String s6 = "3,3";
-    String s7 =" 4.4";
-    String s8 = "5";
-    String s9 = "";
+    final String s0 = "";
+    final String s1 = "a";
+    final String s2 = "A";
+    final String s3 = "!";
+    final String s4 = ",1";
+    final String s5 = "2-";
+    final String s6 = "3,3";
+    final String s7 =" 4.4";
+    final String s8 = "5";
+    final String s9 = "";
 
-    String YYYY_MM_DD_true_1 = "2020-01-01";
-    String YYYY_MM_DD_true_2 = "1990-12-31";
-    String YYYY_MM_DD_false_0 = "";
-    String YYYY_MM_DD_false_1 = "2020-00-01";
-    String YYYY_MM_DD_false_2 = "2020-13-01";
-    String YYYY_MM_DD_false_3 = "2020-01-00";
-    String YYYY_MM_DD_false_4 = "100-01-01";
-    String YYYY_MM_DD_false_5 = "10000-01-01";
-    String YYYY_MM_DD_false_6 = "2000-1-1";
-    String YYYY_MM_DD_false_7 = "2000-01-1";
-    String YYYY_MM_DD_false_8 = "2000-1-01";
-    String YYYY_MM_DD_false_9 = "0200-01-01";
+    final String YYYY_MM_DD_true_1 = "2020-01-01";
+    final String YYYY_MM_DD_true_2 = "1990-12-31";
+    final String YYYY_MM_DD_false_0 = "";
+    final String YYYY_MM_DD_false_1 = "2020-00-01";
+    final String YYYY_MM_DD_false_2 = "2020-13-01";
+    final String YYYY_MM_DD_false_3 = "2020-01-00";
+    final String YYYY_MM_DD_false_4 = "100-01-01";
+    final String YYYY_MM_DD_false_5 = "10000-01-01";
+    final String YYYY_MM_DD_false_6 = "2000-1-1";
+    final String YYYY_MM_DD_false_7 = "2000-01-1";
+    final String YYYY_MM_DD_false_8 = "2000-1-01";
+    final String YYYY_MM_DD_false_9 = "0200-01-01";
 
-    String version_true_1 = "1";
-    String version_true_2 = "2.2";
-    String version_true_3 = "3.3.3";
-    String version_false_0 = "";
-    String version_false_1 = "v1";
-    String version_false_2 = "v2.2";
-    String version_false_3 = "v3.3.3";
-    String version_false_4 = "version 1";
-    String version_false_5 = "1,1";
-    String version_false_6 = "1,1";
+    final String version_true_1 = "1";
+    final String version_true_2 = "2.2";
+    final String version_true_3 = "3.3.3";
+    final String version_false_0 = "";
+    final String version_false_1 = "v1";
+    final String version_false_2 = "v2.2";
+    final String version_false_3 = "v3.3.3";
+    final String version_false_4 = "version 1";
+    final String version_false_5 = "1,1";
+    final String version_false_6 = "1,1";
 
-    String clean_true_1 = "aasdukrjiklmklv";
-    String clean_true_2 = "SDFGHJKKKKKMNBVCFGHJK";
-    String clean_true_3 = "084375887498234756";
-    String clean_true_4 = "SDFfgjhFGJHhlvbmk";
-    String clean_true_5 = "SDF946fgjh6794FGJHhlvb468mk";
-    String clean_true_6 = "TEST_test-94300";
-    String clean_true_7 = "-TEST_test-94300";
-    String clean_true_8 = "_TEST_test-94300";
-    String clean_false_1 = "test test";
-    String clean_false_2 = "test/";
-    String clean_false_3 = "test?";
-    String clean_false_4 = "test&";
-    String clean_false_5 = "test%";
-    String clean_false_6 = "test&";
-    String clean_false_7 = "Robert'); DROP TABLE Students;--";
-    String clean_false_8 = "test'";
-    String clean_false_9 = "test\"";
-    String clean_false_10 = "test;";
-    String clean_false_11 = "test:";
-    String clean_false_12 = "test{}[]<>^^¨¨~=¤#@!|§";
+    final String clean_true_1 = "aasdukrjiklmklv";
+    final String clean_true_2 = "SDFGHJKKKKKMNBVCFGHJK";
+    final String clean_true_3 = "084375887498234756";
+    final String clean_true_4 = "SDFfgjhFGJHhlvbmk";
+    final String clean_true_5 = "SDF946fgjh6794FGJHhlvb468mk";
+    final String clean_true_6 = "TEST_test-94300";
+    final String clean_true_7 = "-TEST_test-94300";
+    final String clean_true_8 = "_TEST_test-94300";
+    final String clean_false_1 = "test test";
+    final String clean_false_2 = "test/";
+    final String clean_false_3 = "test?";
+    final String clean_false_4 = "test&";
+    final String clean_false_5 = "test%";
+    final String clean_false_6 = "test&";
+    final String clean_false_7 = "Robert'); DROP TABLE Students;--";
+    final String clean_false_8 = "test'";
+    final String clean_false_9 = "test\"";
+    final String clean_false_10 = "test;";
+    final String clean_false_11 = "test:";
+    final String clean_false_12 = "test{}[]<>^^¨¨~=¤#@!|§";
 
     @Test
     void isYearMonthDay() {
@@ -151,6 +153,14 @@ class UtilsTest {
 
     @Test
     void cleanSubsetVersion() {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode subset = mapper.createObjectNode();
+        subset.put(Field.VERSION, version_true_1);
+        assertEquals("1", Utils.cleanSubsetVersion(subset).get(Field.VERSION).asText());
+        subset.put(Field.VERSION, version_true_2);
+        assertEquals("2", Utils.cleanSubsetVersion(subset).get(Field.VERSION).asText());
+        subset.put(Field.VERSION, version_true_3);
+        assertEquals("3", Utils.cleanSubsetVersion(subset).get(Field.VERSION).asText());
     }
 
     @Test
