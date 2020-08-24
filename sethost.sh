e48c7e7a335236901fd64ecf81b92e912b706376
Olve Drageset <olve.drageset@gmail.com>
2020-08-24 13:31:04 +0200

    print logs of lds and postgresdb services

--- sethost.sh
+++ sethost.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 sleep 10
+
+echo "docker ps: "
 docker ps
+
+echo "lds logs: "
+docker-compose -f docker-compose.ci.build.yml logs lds
+
+echo "postgresdb logs: "
+docker-compose -f docker-compose.ci.build.yml logs postgresdb
+
 host=localhost
 if grep '^1:name=systemd:/docker/' /proc/1/cgroup
 then
