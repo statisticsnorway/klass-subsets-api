3ab7774341f2cb2223e80e5c72442d3fc9903979
Olve Drageset <olve.drageset@gmail.com>
2020-08-26 16:02:34 +0200

    some unit tests for get all subsets

--- src/test/java/no/ssb/subsetsservice/SubsetsControllerTest.java
+++ src/test/java/no/ssb/subsetsservice/SubsetsControllerTest.java
@@ -14,11 +14,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class SubsetsControllerTest {
 
-
     private static final Logger LOG = LoggerFactory.getLogger(SubsetsServiceApplicationTests.class);
 
     @Test
-    void getSubsetsUrnOnly() {
+    void getSubsetsUrnOnlyCheckStatusOK() {
         SubsetsController instance = SubsetsController.getInstance();
         assertNotNull(instance);
         ResponseEntity<JsonNode> subsets = instance.getSubsets(true, true, true);
@@ -26,20 +25,64 @@ class SubsetsControllerTest {
     }
 
     @Test
-    void getAllSubsets() {
+    void getSubsetsUrnOnlyCheckContainsURNAndRank() {
+        SubsetsController instance = SubsetsController.getInstance();
+        assertNotNull(instance);
+        ResponseEntity<JsonNode> subsets = instance.getSubsets(true, true, true);
+        assertEquals(HttpStatus.OK, subsets.getStatusCode());
+        JsonNode body = subsets.getBody();
+        assertNotNull(body);
+        assertTrue(body.isArray());
+        ArrayNode bodyArrayNode = (ArrayNode) body;
+        System.out.println("Size of list of subsets: "+bodyArrayNode.size());
+        for (JsonNode subsetJsonNode : bodyArrayNode) {
+            JsonNode codesList = subsetJsonNode.get(Field.CODES);
+            assertTrue(codesList.isArray());
+            ArrayNode codesListArrayNode = (ArrayNode) codesList;
+            for (JsonNode rankedURN : codesListArrayNode) {
+                assertTrue(rankedURN.has(Field.URN));
+                assertTrue(rankedURN.has(Field.RANK));
+            }
+        }
+    }
+
+    @Test
+    void getAllSubsetsCheckResponseNotNull() {
         SubsetsController instance = SubsetsController.getInstance();
         assertNotNull(instance);
         ResponseEntity<JsonNode> response = instance.getSubsets(true, true, false);
+        assertNotNull(response);
+    }
+
+    @Test
+    void getAllSubsetsCheckStatusOK() {
+        SubsetsController instance = SubsetsController.getInstance();
+        ResponseEntity<JsonNode> response = instance.getSubsets(true, true, false);
+        assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void getAllSubsetsCheckResponseBodyNotNull() {
+        SubsetsController instance = SubsetsController.getInstance();
+        ResponseEntity<JsonNode> response = instance.getSubsets(true, true, false);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        JsonNode body = response.getBody();
+        assertNotNull(body);
+    }
+
+    @Test
+    void getAllSubsetsCheckBodyIsArray() {
+        SubsetsController instance = SubsetsController.getInstance();
+        ResponseEntity<JsonNode> response = instance.getSubsets(true, true, false);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        JsonNode body = response.getBody();
+        assertTrue(body.isArray());
 
         System.out.println("RESPONSE HEADERS:");
         System.out.println(response.getHeaders());
         System.out.println("RESPONSE BODY");
         System.out.println(response.getBody());
-
-        JsonNode body = response.getBody();
-        assertNotNull(body);
-        assertTrue(body.isArray());
     }
 
     @Test
@@ -48,7 +91,7 @@ class SubsetsControllerTest {
 
         System.out.println("STATUS CODE");
         System.out.println(response.getStatusCodeValue());
-        assertEquals(400, response.getStatusCodeValue());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
         System.out.println("RESPONSE HEADERS:");
         System.out.println(response.getHeaders());
         System.out.println("RESPONSE BODY");
@@ -61,7 +104,7 @@ class SubsetsControllerTest {
 
         System.out.println("STATUS CODE");
         System.out.println(response.getStatusCodeValue());
-        assertEquals(404, response.getStatusCodeValue());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         System.out.println("RESPONSE HEADERS:");
         System.out.println(response.getHeaders());
         System.out.println("RESPONSE BODY");
@@ -74,7 +117,7 @@ class SubsetsControllerTest {
 
         System.out.println("STATUS CODE");
         System.out.println(response.getStatusCodeValue());
-        assertEquals(404, response.getStatusCodeValue());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         System.out.println("RESPONSE HEADERS:");
         System.out.println(response.getHeaders());
         System.out.println("RESPONSE BODY");
@@ -86,10 +129,15 @@ class SubsetsControllerTest {
         ResponseEntity<JsonNode> response = SubsetsController.getInstance().getSubsets(true, true, false);
 
         System.out.println("All subsets:");
-        System.out.println(response.getBody());
+        JsonNode responseBody = response.getBody();
+        System.out.println(responseBody);
+        assertNotNull(responseBody);
+        assertTrue(responseBody.isArray());
         System.out.println("IDs:");
-        for (JsonNode jsonNode : response.getBody()) {
-            JsonNode subset = SubsetsController.getInstance().getSubset(jsonNode.get("id").asText(), true, true, false).getBody();
+        for (JsonNode jsonNode : responseBody) {
+            String id = jsonNode.get(Field.ID).asText();
+            JsonNode subset = SubsetsController.getInstance().getSubset(id, true, true, false).getBody();
+            assert subset != null : "getSubset "+id+" did not return any result";
             assertTrue(subset.has(Field.ID));
             assertEquals(subset.get(Field.ID).asText(), jsonNode.get(Field.ID).asText());
             System.out.println(subset.get(Field.ID));
