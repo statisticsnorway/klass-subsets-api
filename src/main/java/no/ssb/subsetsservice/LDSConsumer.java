2b70556bec0af91f14e43e6a4d247f6920ce030a
Olve Drageset <olve.drageset@gmail.com>
2020-09-04 13:52:48 +0200

    remove superfluous dependencies

--- src/main/java/no/ssb/subsetsservice/LDSConsumer.java
+++ src/main/java/no/ssb/subsetsservice/LDSConsumer.java
@@ -14,11 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.Collections;
