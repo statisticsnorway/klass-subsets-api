840636c0f57b674598241a0633da017da1a9f924
Olve Drageset <olve.drageset@gmail.no>
2021-06-25 15:22:52 +0200

    postgres post serie, get series

--- src/main/java/no/ssb/subsetsservice/PostgresFacade.java
+++ src/main/java/no/ssb/subsetsservice/PostgresFacade.java
@@ -1,14 +1,20 @@
 package no.ssb.subsetsservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.postgresql.util.PGobject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.sql.*;
 
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 public class PostgresFacade implements BackendInterface {
 
@@ -16,6 +22,7 @@ public class PostgresFacade implements BackendInterface {
     private String JDBC_PS_URL = "jdbc:postgresql://localhost:5432/postgres";
     private String USER = "postgres";
     private String PASSWORD = "postgres";
+
 
     private static String getURLFromEnvOrDefault() {
         return System.getenv().getOrDefault("JDBC_PS_URL", "jdbc:postgresql://localhost:5432/postgres");
@@ -32,15 +39,15 @@ public class PostgresFacade implements BackendInterface {
     @Override
     public ResponseEntity<JsonNode> initializeBackend() {
         JDBC_PS_URL = getURLFromEnvOrDefault();
-        USER = "postgres";
-        PASSWORD = "postgres";
+        USER = getUserFromEnvOrDefault();
+        PASSWORD = getPasswordFromEnvOrDefault();
 
-        try (Connection con = DriverManager.getConnection(JDBC_PS_URL, USER, PASSWORD);
-             Statement st = con.createStatement();
-             ResultSet rs = st.executeQuery("SELECT VERSION()")) {
-
+        try {
+            Connection con = DriverManager.getConnection(JDBC_PS_URL, USER, PASSWORD);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT VERSION()");
             if (rs.next()) {
-                System.out.println(rs.getString(1));
+                System.out.println("'SELECT VERSION()' result : "+rs.getString(1));
             }
 
         } catch (SQLException ex) {
@@ -48,6 +55,7 @@ public class PostgresFacade implements BackendInterface {
         }
 
         //TODO: Send the subsetsCreate.sql query to the PostgreSQL server
+
         return new ResponseEntity<>(OK);
     }
 
@@ -58,7 +66,23 @@ public class PostgresFacade implements BackendInterface {
 
     @Override
     public ResponseEntity<JsonNode> getSubsetSeries(String id) {
-        return null;
+        try {
+            Connection con = DriverManager.getConnection(JDBC_PS_URL, USER, PASSWORD);
+            PreparedStatement pstmt = con.prepareStatement("SELECT 'seriesJSON' FROM series");
+            ResultSet rs = pstmt.executeQuery();
+            ObjectMapper om = new ObjectMapper();
+            ArrayNode series = om.createArrayNode();
+            while (rs.next()) {
+                series.add(om.readTree(rs.getString(1)));
+            }
+            return new ResponseEntity<JsonNode>(series, OK);
+        } catch (SQLException ex) {
+            LOG.error("Failed to create series", ex);
+            return ErrorHandler.newHttpError("Failed to create series", INTERNAL_SERVER_ERROR, LOG);
+        } catch (JsonProcessingException e) {
+            LOG.error("Failed to parse json", e);
+            return ErrorHandler.newHttpError("Failed to parse json", INTERNAL_SERVER_ERROR, LOG);
+        }
     }
 
     @Override
@@ -78,7 +102,24 @@ public class PostgresFacade implements BackendInterface {
 
     @Override
     public ResponseEntity<JsonNode> createSubsetSeries(JsonNode subset, String id) {
-        return null;
+        try {
+            Connection con = DriverManager.getConnection(JDBC_PS_URL, USER, PASSWORD);
+            PreparedStatement pstmt = con.prepareStatement("insert into series values(?, ?::JSON)");
+            pstmt.setString(1, id);
+            PGobject jsonObject = new PGobject();
+            jsonObject.setType("json");
+            jsonObject.setValue(subset.toString());
+            pstmt.setObject(2, jsonObject);
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                LOG.debug("insert into series affected "+affectedRows+" rows");
+                return new ResponseEntity<>(CREATED);
+            }
+            return ErrorHandler.newHttpError("No rows were affected", INTERNAL_SERVER_ERROR, LOG);
+        } catch (SQLException ex) {
+            LOG.error("Failed to create series", ex);
+            return ErrorHandler.newHttpError("Failed to create series", INTERNAL_SERVER_ERROR, LOG);
+        }
     }
 
     @Override
