9ee75fa3025aee9c31f5e21d9d1ee52356356928
Olve Drageset <olve.drageset@gmail.no>
2021-10-29 15:47:52 +0200

    clarify health check

--- src/main/java/no/ssb/subsetsservice/controller/HealthController.java
+++ src/main/java/no/ssb/subsetsservice/controller/HealthController.java
@@ -3,6 +3,8 @@ package no.ssb.subsetsservice.controller;
 import no.ssb.subsetsservice.service.DatabaseFactory;
 import no.ssb.subsetsservice.service.DatabaseInterface;
 import no.ssb.subsetsservice.util.KlassURNResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(HealthController.class);
 
     private static HealthController instance;
 
@@ -28,13 +33,23 @@ public class HealthController {
 
     @GetMapping("/health/ready")
     public ResponseEntity<String> ready() {
+        LOG.debug("starting ready check. Pinging KLASS");
         boolean klassReady = new KlassURNResolver().pingKLASSClassifications();
+        LOG.debug("klass ready: "+klassReady);
+        LOG.debug("getting database instance");
         DatabaseInterface database = DatabaseFactory.getDatabase(DatabaseFactory.DEFAULT_DATABASE);
-        database.initializeDatabase();
+        LOG.debug("checking readiness of database");
         boolean databaseReady = database.healthReady();
+        LOG.debug("database ready: "+databaseReady+". Now checking schema presence.");
         boolean schemaPresent = database.getSubsetSeriesSchema().getStatusCode().equals(HttpStatus.OK);
+        LOG.debug("schema present: "+schemaPresent);
         if (klassReady && databaseReady && schemaPresent)
             return new ResponseEntity<>("The service is ready!", HttpStatus.OK);
-        return new ResponseEntity<>("The service is not ready yet.\n KLASS ready: "+klassReady+" \n", HttpStatus.SERVICE_UNAVAILABLE);
+        String readinessDescription = "The service is not ready yet. "+
+                "KLASS ready: "+klassReady+". "+
+                "databaseReady: "+databaseReady+". "+
+                "schemaPresent: "+schemaPresent+". ";
+        LOG.warn(readinessDescription);
+        return new ResponseEntity<>(readinessDescription, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
