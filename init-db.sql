88590278fe1dc40b0755af77737e35d542167a7d
fenglin-ssb <fnh@ssb.no>
2021-11-01 19:37:34 +0100

    update

--- init-db.sql
+++ init-db.sql
@@ -1,7 +1,7 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 -- subsets Access
-create USER subsets with PASSWORD 'subsets';
+create USER subsets with PASSWORD 'postgres';
 create DATABASE subsets;
 grant all privileges on DATABASE subsets TO subsets;
 alter role subsets SUPERUSER;
--- src/test/java/no/ssb/subsetsservice/SubsetsServiceApplicationTests.java
+++ src/test/java/no/ssb/subsetsservice/SubsetsServiceApplicationTests.java
@@ -1,21 +1,14 @@
 package no.ssb.subsetsservice;
-import no.ssb.subsetsservice.entity.SQL;
-import no.ssb.subsetsservice.service.ConnectionPool;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 @SpringBootTest
 public class SubsetsServiceApplicationTests {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SubsetsServiceApplicationTests.class);
+
 
 	@Test
 	void logTest(){
