0d11f20217e75f4bc257a99dacbb2d02a7386858
Olve Drageset <olve.drageset@gmail.no>
2021-11-01 13:24:39 +0100

    add where clause to series insert

--- src/main/java/no/ssb/subsetsservice/entity/SQL.java
+++ src/main/java/no/ssb/subsetsservice/entity/SQL.java
@@ -31,7 +31,7 @@ public class SQL {
     public static String SELECT_SERIES_BY_ID = "SELECT series.series_json FROM series WHERE series.series_id = ?;";
     public static String SELECT_ALL_SERIES = "SELECT series.series_json FROM series;";
     public static String UPDATE_SERIES = "UPDATE series SET series_json = ? WHERE series_id = ?;";
-    public static String ADD_VERSION_TO_SERIES = "UPDATE series SET series_json = jsonb_set(series_json, '{versions,99999}'::text[], to_jsonb(?::text), true);";
+    public static String ADD_VERSION_TO_SERIES = "UPDATE series SET series_json = jsonb_set(series_json, '{versions,99999}'::text[], to_jsonb(?::text), true) WHERE series_id = ?;";
 
     public static String SELECT_VERSION_BY_ID = "SELECT versions.version_json FROM versions WHERE versions.version_id = ?;";
     public static String SELECT_VERSIONS_BY_SERIES = "SELECT versions.version_json FROM versions WHERE versions.series_id = ?;";
